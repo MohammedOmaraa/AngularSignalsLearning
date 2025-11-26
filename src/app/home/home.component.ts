e657bd1663a20f054369e59a15c331ace25dc5d4
MohammedOmaraa <mohamed.ali.omaraa@gmail.com>
2025-11-26 22:02:52 +0200

    simple signal & make signal writable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,23 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { Component, signal } from '@angular/core';
+
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  standalone: true,
+  selector: 'home',
+  imports: [MatTabGroup, MatTab],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  counter = signal(10);
+  counterSignalReadonly = signal(10).asReadonly();
+  increment() {
+    this.counter.set(this.counter());
+    this.counter.update((current) => current + 1);
 
+    // Error
+    // this.counterSignalReadonly.set(this.counter())
+    // this.counterSignalReadonly.update((current) => current + 1);
+  }
 }
